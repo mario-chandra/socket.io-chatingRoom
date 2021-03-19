e5d0a8a1bc8a374fee8dbf2fa4890135adc94837
mario-chandra <mariochandra020@gmail.com>
2021-03-19 20:05:34 +0700

    tugas

---
+++ Readme.md
@@ -0,0 +1,23 @@
+## Chating room using Javascript & socket io
+
+### Requirement
+- Npm or yarn
+
+
+### Setup
+- jalankan yarn 
+- jalankan sever dengan yarn devStart
+- buka 2 client yang terdapat di index.html
+
+### Penjelasan app 
+- saat masuk client akan meminta menginput nama
+- kemudian akan terhubung ke server
+- setelah terhubung maka client dapat saling mengirimkan pesan
+- dan jika salah 1 client di tutup maka akan terputus dari server
+
+### Dependencies
+
+- Socketio
+
+
+
---
+++ script.js
@@ -0,0 +1,34 @@
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('masukan nama anda')
+appendMessage('Terhubung')
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} terhubung`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} terputus`)
+})
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
